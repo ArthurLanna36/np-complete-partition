d675247ed3df593ddab6691d9ad23723b255a0db
Arthur <arthur.galanna36@gmail.com>
2024-11-20 16:05:02 -0300

    Tabulation bottom-up partition (dynamic programming)algorithm implemented

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import numpy as np
 
 from Partition_Algorithms.partition_standard import equal_partition
+from Partition_Algorithms.partition_tabulation_bottom_up import equal_partition_bottom_up
 
-array = np.random.randint(1, 20, size=10)
+array = np.random.randint(1, 20, size=50)
 print(array)
 
-if equal_partition(array):
+# if equal_partition(array):
+#     print("True")
+# else:
+#     print("False")
+
+if equal_partition_bottom_up(array):
     print("True")
 else:
     print("False")
