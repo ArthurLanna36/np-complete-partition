3b93d550371e6edd9d20b71420b8480f9559c5d8
Arthur <arthur.galanna36@gmail.com>
2024-11-24 01:55:36 -0300

    Multiple length tests

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-import numpy as np
-
 from run_single_length_tests import run_single_length_tests
+from run_multiple_length_tests import run_multiple_length_tests
+
+from Partition_Algorithms.partition_standard import equal_partition
+from Partition_Algorithms.partition_memorized_top_down import equal_partition_top_down
+from Partition_Algorithms.partition_tabulation_bottom_up import equal_partition_bottom_up
+from Partition_Algorithms.partition_bottom_up_optimization import equal_partition_bottom_up_optimized
 
 algorithmOption = 1
-testsQuantity = 100
-arrayLength = 30
+testType = 2
 
 match algorithmOption:
     case 1:
-        outputFileName = "equal_partition_results"
+        singleLengthResultFile = "single_length_equal_partition"
+        multipleLengthResultFile = "multiple_length_equal_partition"
+        algorithm = equal_partition
     case 2:
-        outputFileName = "memorizes_top_down_results"
+        singleLengthResultFile = "single_length_memorizes_top_down"
+        multipleLengthResultFile = "multiple_length_memorizes_top_down"
+        algorithm = equal_partition_top_down
     case 3:
-        outputFileName = "tabulation_bottom_up_results"
+        singleLengthResultFile = "single_length_tabulation_bottom_up"
+        multipleLengthResultFile ="multiple_length_tabulation_bottom_up"
+        algorithm = equal_partition_bottom_up
     case 4:
-        outputFileName = "bottom_up_optimized_results"
+        singleLengthResultFile = "single_length_bottom_up_optimized"
+        multipleLengthResultFile ="multiple_length_bottom_up_optimized"
+        algorithm = equal_partition_bottom_up_optimized
     case _: raise ValueError("Invalid algorithm option. Choose a value between 1 and 4.")
 
-run_single_length_tests(algorithmOption, testsQuantity, arrayLength, outputFileName)
-print(f"Tests are finished. Results saved in {outputFileName}.csv")
+if testType == 1:
+    arrayLength = 30
+    testsQuantity = 100
+    run_single_length_tests(algorithm, testsQuantity, arrayLength, singleLengthResultFile)
+    print(f"Tests are finished. Results saved in {singleLengthResultFile}.csv")
+elif testType == 2:
+    arrayLengths = [10, 20, 30, 40, 50]
+    testsQuantityPerLength = 100
+    run_multiple_length_tests(algorithm, testsQuantityPerLength, arrayLengths, multipleLengthResultFile)
+    print(f"Tests are finished. Results saved in {multipleLengthResultFile}.csv")
---
+++ run_multiple_length_tests.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import time
+
+# Function to execute the tests for multiple array lengths
+def run_multiple_length_tests(algorithm, tests_quantity_per_length, array_lengths, output_file):
+    results = []
+
+    # Iterate over the specified array lengths
+    for array_length in array_lengths:
+        # Execute a specified number of tests for each array length
+        for test_num in range(1, tests_quantity_per_length + 1):
+            # Generate random array
+            array = np.random.randint(1, 100, size=array_length)
+
+            # Capture algorithm's execution time
+            start_time = time.perf_counter()
+            result = algorithm(array)
+            end_time = time.perf_counter()
+            elapsed_time = round(end_time - start_time, 10)
+
+            # Save test result
+            test_result = {
+                "Test number": test_num,
+                "Array size": len(array),
+                "Result": result,
+                "Execution time (s)": elapsed_time
+            }
+            results.append(test_result)
+
+    # Save all test results in a CSV
+    df = pd.DataFrame(results)
+    df["Execution time (s)"] = df["Execution time (s)"].map(lambda x: f"{x:.10f}")
+    df.to_csv(f"{output_file}.csv", index=False)
