3a0fed2c086b9356bc2e2747d1321bbf5dbb1794
rafael0121 <rafaelramosdeandrade20@gmail.com>
2024-12-03 16:24:17 -0300

    Update report files

--- plot_graphs.py
+++ plot_graphs.py
@@ -13,53 +13,36 @@ def parse_file(filename):
     df = pd.read_csv(path, sep=',')
     return df
 
-def plot_means(files):
-    color_list = ['g', 'r', 'c', 'm', 'y', 'k']
-    for filename in files:
+def plot_means(filename, color):
+    # Parse file
+    df = parse_file(filename)
 
-        color = color_list.pop()
+    # Get collumns
+    df = df[["Array size", "Execution time (s)"]]
 
-        # Parse file
-        df = parse_file(filename)
+    # Group by Array Size and calc means
+    df = df.groupby("Array size")['Execution time (s)'].mean().reset_index()
 
-        # Get collumns
-        df = df[["Array size", "Execution time (s)"]]
+    # Divide arrays
+    array_size = df["Array size"]
+    exec_time = df["Execution time (s)"]
 
-        # Group by Array Size
-        df = df.groupby("Array size")['Execution time (s)'].mean().reset_index()
+    # Plot datas
+    plt.plot(array_size, exec_time, label=filename, color=color)
 
-        # Divide arrays
-        array_size = df["Array size"]
-        exec_time = df["Execution time (s)"]
+    plt.scatter(array_size, exec_time, color=color, alpha=0.2, marker='o')  # Line plotj
 
-        # Plot datas
-        plt.plot(array_size, exec_time, label=filename, color=color)
-
-        plt.scatter(array_size, exec_time, color=color, alpha=0.2, marker='o')  # Line plotj
-
-        # Plot points
-        if (filename == "Multiple_Length_Tests/multiple_length_bottom_up_optimized"): 
-            for i in df.index:
-                if (i%2 == 0):
-                    plt.text(array_size[i],
-                            exec_time[i] * 1.2,
-                            f'{exec_time[i]:.5f}',
-                            ha='center',
-                            va='bottom',
-                            color=color[0],
-                            fontsize=10,
-                            rotation=65)
-
-        if (filename == "Multiple_Length_Tests/multiple_length_equal_partition"):
-            for i in df.index:
-                if (i%2 == 0):
-                    plt.text(array_size[i],
-                            exec_time[i] * 1.2,
-                            f'{exec_time[i]:.5f}',
-                            ha='center',
-                            va='bottom',
-                            color=color[0],
-                            fontsize=10)
+    # Plot points
+    for i in df.index:
+        if (i%10 == 0):
+            plt.text(array_size[i],
+                    exec_time[i] * 1.2,
+                    f'{exec_time[i]:.5f}',
+                    ha='center',
+                    va='bottom',
+                    color=color[0],
+                    fontsize=5,
+                    rotation=65)
 
     plt.ylabel('Execution time (s)')
     plt.xlabel('Array size')
@@ -67,9 +50,15 @@ def plot_means(files):
     plt.legend()
     plt.grid(True)
 
-    plt.yscale('log')
+    # plt.yscale('log')
 
     path =  "Tests/" + filename
-    plt.show()
+    plt.savefig(path, dpi=300)
+    plt.close()
 
-plot_means(files)
+color_list = ['g', 'r', 'c', 'm', 'y', 'k']
+
+
+for filename in files:
+    color = color_list.pop()
+    plot_means(filename, color)
